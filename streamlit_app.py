87889288ad4e407f98eac39a2122322ef4741242
konjiwale123 <125033254+konjiwale123@users.noreply.github.com>
2023-02-10 05:32:05 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('my parents new healthy diner')
 streamlit.header('Breakfast menu')
-streamlit.text('omega 3 & bluberry otameal')
-streamlit.text('kale,spinach & rocket smoothie')
-streamlit.text('Hard-boiled Free-Range Egg')               
+streamlit.text('🥣 omega 3 & bluberry otameal')
+streamlit.text('🥗 kale,spinach & rocket smoothie')
+streamlit.text('🐔 Hard-boiled Free-Range Egg')               
+streamlit.text('🥑🍞 Avocado Toast')
